482970fda8fc13f69ea66e27789ab65bc5ea3caa
Gooxey <dennis.tiderko@gmail.com>
2022-11-13 19:23:56 +0100

    Added a graphic to visualize the communicator

--- src/communicator/mod.rs
+++ src/communicator/mod.rs
@@ -1,5 +1,34 @@
 //! This module represents the [`proxy's communicator`](Communicator). \
-//! It accepts new connections from the MCManage network and manages the sending and receiving of [`messages`](mcm_misc::message::Message).
+//! It accepts new connections from the MCManage network and manages the sending and receiving of [`messages`](mcm_misc::message::Message).\
+//! \
+//! ```text
+//! -------------------------------------
+//! |           Communicator            |
+//! |                                   |
+//! |    ----------------------------   |
+//! |    | Communicator main thread |   |   Creates a handler for each new connection.
+//! |    ----------------------------   |
+//! |         |                |        |
+//! |         |                |        |
+//! |         V                V        |
+//! |    -----------      -----------   |   Send and receive messages to and from their 
+//! |    | Handler |      | Handler |   |   connected client.
+//! |    -----------      -----------   |   A client could be a Runner or a Client application.
+//! |      Λ                      Λ     |
+//! |      |                      |     |   Send and receive channels to transmit messages.
+//! |      |                      |     |
+//! |      |     ------------     |     |   Passes on received messages to the right receiver.
+//! |      ----> | InterCom | <----     |   This can be a handler or the console.
+//! |            ------------           |   
+//! |                 Λ                 |
+//! |                 |                 |
+//! ----------------- | -----------------   Send and receive channels to transmit messages.
+//!                   |
+//!                   V
+//! -------------------------------------   Here, all received commands get executed and
+//! |              Console              |   messages for clients are created.
+//! -------------------------------------
+//! ```
 
 use self::intercom::InterCom;
 
